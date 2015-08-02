ede073209822b07f68e10b15b5efcdecd1bbbf37
Jesus <chuy8jay@gmail.com>
2015-08-01 21:24:31 -0600

    Added Transparent Activity

---
+++ mobile/src/main/java/com/zarate/jesus/drinkwater/Settings/SettingsActivity.java
@@ -0,0 +1,10 @@
+package com.zarate.jesus.drinkwater.Settings;
+
+import android.app.Activity;
+
+/**
+ * Created by Jesus Zarate on 8/1/15.
+ */
+public class SettingsActivity extends Activity
+{
+}
